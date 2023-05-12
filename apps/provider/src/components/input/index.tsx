35a1026a87f6585e04b5077fd317fad7b4935f9d
bbamii <bbamii@outlook.com>
2023-05-12 21:41:14 +0100

    fix wrong type in input component

--- apps/provider/src/components/input/index.tsx
+++ apps/provider/src/components/input/index.tsx
@@ -3,12 +3,12 @@
 import React from "react"
 import { Field, FieldProps } from "formik"
 
-export const Input = ({ name, ...props }: React.InputHTMLAttributes<React.HTMLInputTypeAttribute>) => {
+export const Input = ({ name, ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
 	return (
 		<Field name={name}>
 			{({ field, meta }: FieldProps) => (
 				<div>
-					<input name={name} {...props} {...field} />
+					<input {...props} {...field} />
 					{meta.error}
 				</div>
 			)}
