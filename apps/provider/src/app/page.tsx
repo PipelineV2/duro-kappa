4d25d1f110c3324d2c0c2d37ef821ad6788442c6
bbamii <bbamii@outlook.com>
2023-06-01 18:42:03 +0100

    add admin routes

--- apps/provider/src/app/page.tsx
+++ apps/provider/src/app/page.tsx
@@ -1,7 +1,39 @@
-import Image from 'next/image'
+import createForm from "@/components/form";
+import * as Yup from 'yup';
+import { QueueUserInputType } from '@/models/auth';
+
+const validationSchema = Yup.object({
+  email: Yup.string().required(),
+  phone: Yup.string().required(),
+})
 
 export default function Home() {
-	return (
-		<div> this is home </div>
-	)
+  const Form = createForm<QueueUserInputType, typeof validationSchema>({
+    initialValues: {
+      email: "",
+      phone: "",
+    },
+    validationSchema
+  });
+
+  const queue = () => {
+    // api request to join queue.
+    // returns the queue number, and 
+  }
+  return (
+    <div>
+      this is Reginals's store
+
+      <div>
+        queue up!. with your email or phone!
+        a notification will be sent to you when it's almost your turn, and when its your turn.
+      </div>
+
+      <Form submit={queue}>
+        <Form.Input placeholder="email" name="email" type="email" />
+        <Form.Input placeholder="phone" name="phone" type="tel" />
+        <Form.Submit text="submit form!" />
+      </Form>
+    </div>
+  )
 }
--- apps/provider/src/models/auth.ts
+++ apps/provider/src/models/auth.ts
@@ -2,8 +2,13 @@
 export type LoginInputType = {}
 
 export type OnboardingInputType = {
-	business_name: string
-	location: string
+  business_name: string
+  location: string
+}
+
+export type QueueUserInputType = {
+  email: string
+  phone: string
 }
 
 export interface User { }
