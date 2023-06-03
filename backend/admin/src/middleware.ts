6ec1281c2d3b2bf9ef8b034381bbbc330e0da281
bbamii <bbamii@outlook.com>
2023-06-03 05:18:27 +0100

    add admin routes

--- backend/admin/src/middleware.ts
+++ backend/admin/src/middleware.ts
@@ -1,17 +1,42 @@
-import { NextFunction } from "express";
-import { sendError } from "expressapp/src/utils";
+import z from 'zod';
 import log from "logger";
+import validator from "input-validator";
+import databaseClient from "database";
+import { sendError } from 'expressapp/src/utils';
+import { Response, NextFunction } from 'express';
+import { Admin } from 'database/src/models';
 
-export const validator = (schema: { parse: Function }) => {
-  return (req: any, res: any, next: NextFunction) => {
-    try {
-      schema.parse(req.body);
-      return next();
-    } catch (error: any) {
-      log.error(error.message);
-      return sendError(res, "Error occured");
-    }
+export const validation = validator(
+  z.object({
+    "company_name": z.string(),
+    "location": z.string(),
+    "coordinates": z.string()
+  })
+)
+
+export const loginValidator = validator(
+  z.object({
+    "email": z.string(),
+    "password": z.string()
+  })
+)
+
+export const auth = (isSuperAdmin: boolean) => async (req: any & { user: Admin }, res: Response, next: NextFunction) => {
+  const database = databaseClient();
+  try {
+    const { email } = req.body;
+    if (!email)
+      throw new Error(`Invalid authentication!. please provide an email"}`)
+
+    const user = await database.getAdminByEmail(email);
+    if (!user) throw new Error("Admin not found with this email.")
+    if (isSuperAdmin && !user.superAdmin)
+      throw new Error("Invalid authentication! You're not allowed to access this route.");
+
+    req.user = user;
+    return next();
+  } catch (error: any) {
+    log.error(error)
+    return sendError(res, error.message);
   }
 }
-
-
--- backend/admin/src/routes.ts
+++ backend/admin/src/routes.ts
@@ -1,11 +1,10 @@
-import { Router } from 'express';
+import { Router, Response } from 'express';
 import databaseClient from "database";
-import z from 'zod';
-import log from "logger";
-import queueClient, { DURO_QUEUE, DuroQueueOptions, NOTIFICATION_QUEUE } from "queue"
+import queueClient, { DURO_QUEUE, MERCHANT_REGISTRATION_QUEUE } from "queue"
 import { sendError, sendSuccess } from 'expressapp/src/utils';
-import { NotificationOptions } from 'notifications'
-import { validator } from './middleware';
+import { loginValidator, auth } from './middleware';
+import { Admin, Input } from 'database/src/models';
+import log from "logger";
 
 const router = Router();
 const database = databaseClient();
@@ -13,37 +12,91 @@ const queue = queueClient();
 database.connect();
 queue.connect();
 
-const auth = async (req, res, next) => {
-  const { phone, email, name } = req.body;
 
-  if (!phone && !email) return sendError(res, `Invalid authentication!. please provide a${phone ? " phone number" : "n email "}`)
 
-  const user = await database.getUserByEmail(email);
-  req.user = user;
-  next();
-}
-
-const validation = z.object({
-  "company_name": z.string(),
-  "location": z.string(),
-  "coordinates": z.string()
+// admin logins...
+router.use('/login', loginValidator);
+router.post('/login/merchant', auth(true), (_, res) => {
+  return sendSuccess(res, "Successfully logged in.");
 })
+router.post('/login/branch', auth(false), (_, res) => {
+  return sendSuccess(res, "Successfully logged in.");
+});
 
-router.post('/advance-queue', async (req, res) => {
+
+// queue actions
+router.post('/advance-queue', auth(false), async (req: any & Admin, res: Response) => {
   // pop value from queue.
-  // store current atending somewhere.
-  console.log(req, res)
+  try {
+    const { userId: previousAttendedTo } = req.body;
+    const { branchId } = req.user;
+    const branch = await database.getBusinessBranchById(branchId)
+    const userToAttendTo: string | null = await queue.dequeue<any, string>(DURO_QUEUE, { topic: branchId, total: branch.current_attended ?? 0 })
+
+    if (!userToAttendTo)
+      return sendSuccess(res, "There is nobody currently on the queue.");
+
+    // update the branch's currently_attended
+    await database.updateBusinessBranchById(branchId, { current_attended: (branch.current_attended ?? 0) + 1 })
+
+    // update previous users' attending_to and current queue
+    await database.updateUserById(previousAttendedTo, { in_queue: false, attending_to: false, current_queue: "" });
+
+    // update next users' attending_to.
+    await database.updateUserById(userToAttendTo, { attending_to: true, current_queue: branchId })
+
+    // store current atending somewhere.
+    console.log(req, res)
+    return sendSuccess(res, "Suceessfully advanced queue. A Notification will be sent to the next user");
+  } catch (error: any) {
+    return sendError(res, "Error occured while advancing queue.")
+  }
 })
 
-router.get('/current-user', async (req, res) => {
-  // get current user from repository.
+router.get('/currently-attending-to', auth(false), async (req, res) => {
+  // get current user(s) that's being attended to from repository.
+  // get all users with attending_to true, with current_queue for the
+  // current branch
   console.log(req, res)
 });
 
-router.get('/user-queue-position', async (req, res) => {
-  // get user's position on the queue
-  console.log(req, res)
+// SUPERADMIN JOB!
+router.post('/create-branch', auth(true), async (req: any & { user: Admin }, res: any) => {
+  // create a new branch
+  const { location, coordinates } = req.body;
+  const { username, email, password } = req.body as Input<Admin>;
+  const { merchantId } = req.user;
+  const { id, company_name } = await database.getMerchantById(merchantId);
+
+  try {
+    const branch = await database.insertBranch({
+      merchantId,
+      location,
+      coordinates,
+      qr_code: "",
+      slug: `${location.split(" ").join("_").toLowerCase()}__${company_name}`
+    })
+    await database.insertAdmin({
+      merchantId,
+      branchId: branch.id,
+      username, email, password, superAdmin: false
+    })
+    await queue.enqueue(MERCHANT_REGISTRATION_QUEUE, branch.id);
+
+    log.info("successfully added branch to list. :", id)
+    return sendSuccess(res, "Successfully registered your business branch.")
+  } catch (error: any) {
+    log.error(error.message);
+    return sendError(res, error.message)
+  }
 });
+
+// update merchant details
+//router.patch('/merchant', async (req, res) => {
+//  console.log(req, res);
+//})
+
+
 
 export default router;
 
