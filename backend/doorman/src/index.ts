aac0e9f2f5a9e15b15c48a1735eb0e14f5fa8d2d
bbamii <bbamii@outlook.com>
2023-06-12 00:04:25 +0100

    dockerize backend and job servers

--- backend/doorman/src/index.ts
+++ backend/doorman/src/index.ts
@@ -2,12 +2,11 @@ import expressapp from 'expressapp';
 import { Router } from "express";
 import duro from "duro-queue/src/routes";
 import admin from "admin/src/routes";
-import jobs from "jobs/src/index";
 
 const router = Router();
 
+router.get('/', (_, res) => { res.send("Welcome to DURO doorman! happy waiting :)") })
 router.use('/admin', admin);
 router.use('/queue', duro);
-router.use('/jobs', jobs);
 
 expressapp(router)
---
+++ cron.Dockerfile
@@ -0,0 +1,30 @@
+from gplane/pnpm:node18
+
+workdir /app
+
+copy . /app
+
+workdir /app 
+
+run pnpm i
+
+run pnpm run build
+
+workdir /app/shared/database
+
+run npx prisma generate
+
+run pnpm i ts-node -w
+
+env DATABASE_URL postgres://postgres:root@localhost:5432
+
+env QUEUE_CONNECTION_URL redis://red-ci29ksm7avj2t336jnf0:6379 
+
+env PORT 4000
+
+expose 4000
+
+cmd ["npx", "ts-node", "/app/backend/jobs/src/index.ts"]
+
+
+
