5d5e3a46c2e7076b213d71a83a7d347f4948beca
bbamii <bbamii@outlook.com>
2023-06-13 18:09:47 +0100

    update

--- apps/provider/src/middleware.ts
+++ apps/provider/src/middleware.ts
@@ -4,7 +4,7 @@ import { is_user_logged_in } from '@/contexts/auth.context';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log(is_user_logged_in())
+  // console.log(is_user_logged_in())
   //if (!is_user_logged_in())
   //return NextResponse.redirect(new URL('/', request.url));
 
